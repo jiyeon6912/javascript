3178c6d035c706cf7d0be01bdda5d01b19d3bf6c
jiyeon6912 <jiyeon6912@gmail.com>
2020-11-03 11:02:22 +0900

    tutorial

--- tutorial/accordion/app.js
+++ tutorial/accordion/app.js
@@ -2,10 +2,10 @@ const  content = document.querySelectorAll('.content')
 console.log(content)
 
 for(let i=0; i < content.length; i++) {
-  content[i].addEventListener('click',function() {
+  content[i].addEventListener('click',function() {  //content는 여러개의 div이므로 배열형태를 가짐.
     console.log('hahaha')
     console.log(this) //this는 클릭을 한 content div를 가리킨다.
-    
+
     this.classList.toggle('active') //this는 일반형,표준형함수에서만 작동함. arrow(축약형)함수일때는 작동안함.
   })
 }
--- tutorial/scroll_effect/app.js
+++ tutorial/scroll_effect/app.js
@@ -2,6 +2,6 @@ const section = document.querySelector('section')
 
 window.addEventListener('scroll', function() {
   console.log(scrollY)
-  let value = window.scrollY
-  section.style.clipPath = `circle(${value}px at center center)`
+  let value = window.scrollY  //window 에서 scrollY 위,아래 방향으로 일어날때 값이 0보다 큰값으로 증가 됨
+  section.style.clipPath = `circle(${value}px at center center)` //px의 값이 크지면서 보여지는 원영역이 커짐
 })
