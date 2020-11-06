92d3bf92a505d54e467f18179fe87b4b20f204f7
jiyeon6912 <jiyeon6912@gmail.com>
2020-11-06 11:36:48 +0900

    tutorial

--- tutorial/recipe/app.js
+++ tutorial/recipe/app.js
@@ -2,11 +2,35 @@ const form = document.querySelector('form')
 const input = form.querySelector('input')  //input value 값은 form을 통해서 받아온 데이타이다.
 const food_list = document.querySelector('.food_list')
 
+const APP_ID = '9a79b798'
+const APP_KEY = '71183b857c14f64142c0a1ab4fe9e4ac'
+
+// https://api.edamam.com/search
+// https://api.edamam.com/search?q=chicken&app_id=${71183b857c14f64142c0a1ab4fe9e4ac}&app_key=${71183b857c14f64142c0a1ab4fe9e4ac}&from=0&to=3
+
+
+
+ async function getRecipe(query) {
+console.log(query)
+const baseURL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20`
+
+const response = await fetch(baseURL) //url주소에서 데이타를 가져오는 함수
+const data = await response.json()
+console.log(data.hits)
+
+}
+
 function init() {
 console.log('Recipe Start!!')
 form.addEventListener('submit', e => {
   e.preventDefault()  //브라우즈의 기본이벤트로 인해 출력이 안되는것을 방지함
-  console.log('hahaha')
+  const query = input.value 
+  if(query === '') return
+  // console.log(input.value)
+  getRecipe(query)
+  input.value = ''
+
+
 
 }) 
 }
